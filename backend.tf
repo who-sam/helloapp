ab222ec2170e13279af4d54da8ec8f8f3cf3b760
who sam <144911472+who-sam@users.noreply.github.com>
2025-11-21 15:14:35 +0200

    Change S3 bucket name for Terraform backend

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "hello-devops-production-terraform-state"
+    bucket       = "hello-devops-production-terraform-state-who"
     key          = "eks/terraform.tfstate"
     region       = "eu-west-1"
     encrypt      = true
